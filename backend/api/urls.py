2601482bc5beb69fc3132bbd4377231287ba59c6
kwon0144 <kinsey.w5505@gmail.com>
2025-04-17 01:45:45 +1000

    chore: clean up unused code and update requirements.txt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
-from .views import get_users, create_user, get_temperature_data, get_storage_advice, get_food_types, get_foodbanks
+from .views import get_storage_advice, get_food_types, get_foodbanks
 from . import produce_detection
 from .output_calender import generate_calendar, generate_ical
 from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
-    path("users/", get_users, name="get_users"),
-    path("users/create/", create_user, name="create_user"),
     path('detect-produce/', produce_detection.detect_produce, name='detect_produce'),
     path('generate_calendar/', generate_calendar, name='generate-calendar'),
     path('calendar/<uuid:calendar_id>.ics', generate_ical, name='generate-ical'),
-    path('temperature/', get_temperature_data, name='temperature-data'),
     path('storage-advice/', get_storage_advice, name='get_storage_advice'),
     path('food-types/', get_food_types, name='get_food_types'),
     path('foodbanks/', get_foodbanks, name='get_foodbanks'),
