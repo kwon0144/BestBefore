199d459d0c2fda3dc9033ef3e3cc98940620ce6e
jzha0477 <jzha0477@student.monash.edu>
2025-04-17 19:11:29 +1000

    solve a merge problem

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from .views import (
-    get_users, create_user, get_temperature_data, get_storage_advice, 
-    get_food_types, get_foodbanks, get_second_life_items, get_second_life_item_detail
+    get_storage_advice, 
+    get_food_types, 
+    get_foodbanks,
+    get_second_life_items,
+    get_second_life_item_detail
 )
 from . import produce_detection
 from .output_calender import generate_calendar, generate_ical
@@ -9,12 +12,9 @@ from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
-    path("users/", get_users, name="get_users"),
-    path("users/create/", create_user, name="create_user"),
     path('detect-produce/', produce_detection.detect_produce, name='detect_produce'),
     path('generate_calendar/', generate_calendar, name='generate-calendar'),
     path('calendar/<uuid:calendar_id>.ics', generate_ical, name='generate-ical'),
-    path('temperature/', get_temperature_data, name='temperature-data'),
     path('storage-advice/', get_storage_advice, name='get_storage_advice'),
     path('food-types/', get_food_types, name='get_food_types'),
     path('foodbanks/', get_foodbanks, name='get_foodbanks'),
