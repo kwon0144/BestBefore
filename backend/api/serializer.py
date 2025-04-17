f649cdf35c9e1954aa4c1e5ff98d216c5d98e924
kwon0144 <kinsey.w5505@gmail.com>
2025-04-18 02:59:28 +1000

    chore: clean up unused code and update requirements.txt

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,15 +1,5 @@
 from rest_framework import serializers
-from .models import User, Temperature, Geospatial
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-class TemperatureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Temperature
-        fields = '__all__'
+from .models import Geospatial
 
 class FoodBankListSerializer(serializers.ModelSerializer):
     key = serializers.SerializerMethodField()
