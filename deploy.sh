9734cadf8f62507db9de0a885716b2ead4937f7c
kwon0144 <kinsey.w5505@gmail.com>
2025-04-18 19:15:12 +1000

    cicd: change docker image name

--- deploy.sh
+++ deploy.sh
@@ -36,8 +36,8 @@ sudo docker stop $BACKEND_NAME || true
 sudo docker rm $BACKEND_NAME || true
 
 # Pull latest images
-docker pull $DOCKER_USERNAME/react-django-frontend:$FRONTEND_TAG
-docker pull $DOCKER_USERNAME/react-django-backend:$BACKEND_TAG
+docker pull $DOCKER_USERNAME/bestbefore-frontend:$FRONTEND_TAG
+docker pull $DOCKER_USERNAME/bestbefore-backend:$BACKEND_TAG
 
 # Remove old Docker images
 docker image prune -f
@@ -48,11 +48,11 @@ docker run -d --name $FRONTEND_NAME --network app-network -p $FRONTEND_PORT:3000
   -e NEXT_PUBLIC_API_KEY="$NEXT_PUBLIC_API_KEY" \
   -e NEXT_PUBLIC_GOOGLE_MAPS_API_KEY="$NEXT_PUBLIC_GOOGLE_MAPS_API_KEY" \
   -e NEXT_PUBLIC_MAP_ID="$NEXT_PUBLIC_MAP_ID" \
-  $DOCKER_USERNAME/react-django-frontend:$FRONTEND_TAG
+  $DOCKER_USERNAME/bestbefore-frontend:$FRONTEND_TAG
 echo "Frontend deployed successfully on HTTP (port $FRONTEND_PORT)"
 
 # Run backend container
 docker run -d --name $BACKEND_NAME --network app-network -p $BACKEND_PORT:8000 \
   --env-file .env \
-  $DOCKER_USERNAME/react-django-backend:$BACKEND_TAG
+  $DOCKER_USERNAME/bestbefore-backend:$BACKEND_TAG
 echo "Backend deployed successfully (port $BACKEND_PORT)"
