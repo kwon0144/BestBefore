52801c75a2e87c60db65f0cc64082902cea8127e
kwon0144 <kinsey.w5505@gmail.com>
2025-04-29 04:27:05 +1000

    ui: responsiveness for footer

--- frontend/src/app/(components)/Footer/index.tsx
+++ frontend/src/app/(components)/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         {name: "Storage Management"},
         {name: "Expiration Reminder"},
     ]},
-    {name: "Eco Grocery List", link: "/eco-grocery", section: [
+    {name: "Eco Grocery", link: "/eco-grocery", section: [
         {name: "Choices of Meals"},
         {name: "Food Inventory"},
         {name: "Smart Grocery List"},
@@ -27,19 +27,16 @@ const Footer = () => {
   return (
     <footer className="bg-darkgreen text-white py-12">
       <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center mb-10">
-          <div>
-            <div className='px-40 relative h-24 w-52'>
-              <Image 
-                src="https://s3-tp22.s3.ap-southeast-2.amazonaws.com/BestBefore/logo-white.png" 
-                alt="Best Before Logo"
-                fill
-                style={{ objectFit: 'contain' }}
-                sizes="(max-width: 768px) 100vw, 208px"
-              />
-            </div>
-            <div className="mb-6 px-20 md:mb-0 ">
-              <p className="max-w-md">
+        <div className="flex flex-col justify-center mb-10 md:flex-row md:justify-between">
+          <div className='flex flex-col items-center md:items-start md:justify-start md:pl-16'>
+            <Image 
+              src="https://s3-tp22.s3.ap-southeast-2.amazonaws.com/BestBefore/logo-white.png" 
+              alt="Best Before Logo"
+              width={208}
+              height={208}
+            />
+            <div className="max-w-md mb-10 pl-3 md:mb-0 text-center md:text-left">
+              <p>
                 Together for a zero-waste kitchen - save food, money, and our planet with BestBefore
               </p>
             </div>
@@ -51,7 +48,7 @@ const Footer = () => {
                     <p className="underline text-md font-bold mb-4 hover:text-[#A5D6B7] transition-colors duration-300 cursor-pointer">
                       {item.name}
                     </p>
-                    <ul className="space-y-2 text-gray-300 pl-2">
+                    <ul className="space-y-2 text-gray-300 pl-2 hidden xl:block">
                     {item.section.map((section) => (
                       <div key={section.name}>
                           <li>
