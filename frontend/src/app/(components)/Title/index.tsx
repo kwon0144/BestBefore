9b530db79703de770e0e3cdc91294a8f150a8287
kwon0144 <kinsey.w5505@gmail.com>
2025-04-29 05:26:15 +1000

    ui: title animation

--- frontend/src/app/(components)/Title/index.tsx
+++ frontend/src/app/(components)/Title/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 interface TitleProps {
     heading: string;
     description: string;
@@ -5,15 +7,20 @@ interface TitleProps {
 }
 
 export default function Title({heading, description, background}: TitleProps) {
-    return <div
+    return <motion.div
+    initial={{ y: -50, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 1, delay: 1, ease: "easeOut" }}
     style={{
         backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${background})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center'
     }}>
-        <div className="max-w-3xl mx-auto flex flex-col justify-center items-center self-center py-24 text-darkgreen">
+        <div 
+            className="max-w-3xl mx-auto flex flex-col justify-center items-center self-center py-24 text-darkgreen"
+        >
             <h1 className="text-4xl font-bold mb-4 text-center">{heading}</h1>
             <p className="text-lg font-semibold text-center text-center">{description}</p>
         </div>
-    </div>
+    </motion.div>
 }
