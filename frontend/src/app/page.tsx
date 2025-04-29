ba5d2b81893dcea0d35b163315bb84e132965372
kwon0144 <kinsey.w5505@gmail.com>
2025-04-29 19:24:49 +1000

    fix: error for frontend image build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Articles from './(components)/Home/Articles';
-import Hero from './(components)/Home/Hero';
-import ProblemStatement from './(components)/Home/ProblemStatement';
-import Solution from './(components)/Home/Solution';
+import Articles from '@/app/(components)/Home/Articles';
+import Hero from '@/app/(components)/Home/Hero';
+import ProblemStatement from '@/app/(components)/Home/ProblemStatement';
+import Solution from '@/app/(components)/Home/Solution';
 
 export default function Home() {
   return (
