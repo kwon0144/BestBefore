741fda39abd8cee3828cb2f7bd3f890a0fef8b9b
kwon0144 <kinsey.w5505@gmail.com>
2025-04-29 20:24:53 +1000

    fix: backend migration and setting for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Articles from './(components)/home/Articles';
 import Hero from './(components)/home/Hero';
 import ProblemStatement from './(components)/home/ProblemStatement';
 import Solution from './(components)/home/Solution';
+import Articles from './(components)/home/Articles';
 
 export default function Home() {
   return (
@@ -11,6 +11,6 @@ export default function Home() {
       <ProblemStatement />
       <Solution />
       <Articles />
-      </div>
+    </div>
   );
 }
