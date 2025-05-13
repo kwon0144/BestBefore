692c920ac01f386cba4cc7d685ac5dfee90e031f
jzha0477 <jzha0477@student.monash.edu>
2025-05-14 01:24:04 +1000

    Add eslint and next.js config files

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off'
+  }
+} 
